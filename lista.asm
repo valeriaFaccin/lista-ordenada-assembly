075958960a9f317e453a6c1a5c3c504db7385baa
Valéria Faccin <valeriafaccin00@gmail.com>
2024-05-26 15:45:43 -0300

    :tada: commit inicial - lista ordenada

---
+++ lista.asm
@@ -0,0 +1,5 @@
+#	Universidade Federal da Fronteira Sul - Campus Chapecó, SC
+#		Ciências da Computação
+#	Disciplina: Organização de Computadores
+#	Docente: Luciano Lores Caimi
+#	Discentes: João Vitor Gomes da Silva e Valéria Faccin
