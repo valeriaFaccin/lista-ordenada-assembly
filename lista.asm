68a040cfe5be7670b2adb0e1d0a984a79081a338
Valéria Faccin <valeriafaccin00@gmail.com>
2024-06-09 17:54:14 -0300

    :hammer: incluindo separamento entre elementos impressos na função imprimir lista

--- lista.asm
+++ lista.asm
@@ -17,8 +17,10 @@ txt_inserido:	.string "foi inserido no índice: "
 txt_qtd_ins:	.string " número(s) inserido(s)\n"
 txt_removido:	.string "foi removido\n"
 txt_input:	.string "\nDigite um número\n"
+txt_imprimir:	.string	"\nElementos da Lista\n"
 n_implementado:	.string "\nEsta operação não foi implementada\n"
 sem_elementos:	.string	"\nNão há elementos na lista\n"
+espaco:		.string " - "
 txt_invalido:	.string "\n***DIGITE UMA OPÇÃO VÁLIDA***\n"
 
 		#print do menu
@@ -175,23 +177,28 @@ imprime_lista:
 	beqz t0, nenhum_elemento	# Vai para label e t0 = 0
 
 imprimir:
-    mv t1, t0				# Salva em t1 o endereço salvo em t0
-    lw a0, 0(t1)			# Salva em a0 o valor em t1
-    li a7, 1             		# Comando para PrintInteger
-    ecall				# Chama OS
+        mv t1, t0			# Salva em t1 o endereço salvo em t0
+        lw a0, 0(t1)			# Salva em a0 o valor em t1
+        li a7, 1             		# Comando para PrintInteger
+        ecall				# Chama OS
 
-    lw t0, 4(t1)          		# Salva em t0 o proximo endereço da lista
-    beqz t0, fim_imprimir     		# Vai para fim_imprimir se t0 = 0, ou seja, estar no fim da lista
-    j imprimir				# Volta para imprimir
+        lw t0, 4(t1)          		# Salva em t0 o proximo endereço da lista
+        beqz t0, fim_imprimir     	# Vai para fim_imprimir se t0 = 0, ou seja, estar no fim da lista
+    
+        la a0, espaco  
+        li a7, 4              		# Comando para PrintString
+        ecall				# Chama OS
+    
+       j imprimir			# Volta para imprimir
 
 fim_imprimir:
-    ret
+       ret
 
 nenhum_elemento:
-    la a0, sem_elementos  
-    li a7, 4              		# Comando para PrintString
-    ecall				# Chama OS
-    ret
+       la a0, sem_elementos  
+       li a7, 4              		# Comando para PrintString
+       ecall				# Chama OS
+       ret
 
 #################################################################
 # função: void estatistica()					#
